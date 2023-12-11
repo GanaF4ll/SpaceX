0efb4d4dc4772c247b8b103e2dcffb9b20bfa92c
GanaF4ll <ganafall9498@gmail.com>
2023-12-11 11:59:53 +0100

    config app.js

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const express = require("express");
 const nodemon = require("nodemon");
 
 const axios = require("axios");
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/apinode");
+
+const app = express();
+const port = 3004;
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Space X app listening on port ${port}`);
+});

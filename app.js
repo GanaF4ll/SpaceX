0157c3333c81b2e100fee4f19f707ad5c59451a2
GanaF4ll <ganafall9498@gmail.com>
2023-12-11 14:30:15 +0100

    updated database's url (l8)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const nodemon = require("nodemon");
 const axios = require("axios");
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/apinode");
+mongoose.connect("mongodb://127.0.0.1:27017/SpaceX");
 
 const app = express();
 const port = 3004;
